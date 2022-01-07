08f3f58df38e5b154df4870d56ba618ab577a7d9
toslund <timoslund@gmail.com>
2022-01-07 15:43:12 -0800

    add confirmemail.vue

---
+++ src/views/ConfirmEmail.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    Confirming Email
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ConfirmEmail',
+
+  components: {
+  },
+  data() {
+    return {
+      foo: 'bar',
+    };
+  },
+  mounted() {
+    const urlParams = new URLSearchParams(window.location.search);
+    console.log(window.location);
+    console.log(urlParams.has('token')); // true
+    console.log(urlParams.get('token')); // "MyParam"
+  },
+  created() {
+  },
+};
+</script>
